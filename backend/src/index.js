3849574fc1c6c7a28e375be14cb330d0079a83e3
0x7s0lt1 <zsoltfehervari@gmx.com>
2025-08-25 09:59:38 +0200

    refactor: esm to common

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,35 @@
-import { Server } from "socket.io";
-import { createServer } from "http";
+const http = require("http");
+const io = require("socket.io");
 
 const port = process.env.PORT || 4444;
 
+const app = http.createServer();
+
+const server = io(app, {
+    cors: { origin: "*" },
+});
+
 let screenSocket;
+server.on("connection", (socket) => {
 
-(async () => {
+    socket.emit("handshake", { id: socket.id });
 
-    const server = createServer();
-
-    const io = new Server(server, {
-        cors: { origin: "*" },
+    socket.on("screenConnect", () => {
+        screenSocket = socket;
     });
 
-    io.on("connection", (socket) => {
-
-        socket.emit("handshake", { id: socket.id });
-
-        socket.on("screenConnect", () => {
-            screenSocket = socket;
-        });
-
-        socket.on("action", (data) => {
-            if (screenSocket) {
-                screenSocket.emit("action", data);
-            }
-        });
-
-        socket.on("disconnect", () => {
-            if (screenSocket?.id === socket.id) {
-                screenSocket = null;
-            }
-        });
-
+    socket.on("action", (data) => {
+        if (screenSocket) {
+            screenSocket.emit("action", data);
+        }
     });
 
-    server.listen(port);
+    socket.on("disconnect", () => {
+        if (screenSocket?.id === socket.id) {
+            screenSocket = null;
+        }
+    });
 
-})();
+});
+
+server.listen(port);
